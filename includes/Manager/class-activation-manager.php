032c1082dc65ee1ebf6284e90407a3da31bd1893
leouix <nsht22sola@gmail.com>
2026-05-02 22:29:32 +0700

    fix cs

--- includes/Manager/class-activation-manager.php
+++ includes/Manager/class-activation-manager.php
@@ -56,8 +56,7 @@ class Activation_Manager {
 		$charset_collate = $wpdb->get_charset_collate();
 		$table_name      = $wpdb->prefix . 'weal_profile_plugin';
 
-		$sql = /** @lang text */
-			"CREATE TABLE IF NOT EXISTS $table_name (
+		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
             id mediumint(9) NOT NULL AUTO_INCREMENT,
             user_page_url varchar(255) NOT NULL,
             fields_allowed_json varchar(255) NULL,
