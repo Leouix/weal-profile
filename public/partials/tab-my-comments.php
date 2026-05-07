1bf70f7562854557aba5ddbd416442e3bd4993f7
leouix <nsht22sola@gmail.com>
2026-05-08 03:05:24 +0700

    fix cs

--- public/partials/tab-my-comments.php
+++ public/partials/tab-my-comments.php
@@ -15,10 +15,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * User comments variable.
  *
- * @var $user_comments
+ * @var $weal_profile_user_comments
  */
 
-if ( ! empty( $user_comments ) ) {
+if ( ! empty( $weal_profile_user_comments ) ) {
 
 	?>
 	<div class="weal-comment-reactions">
@@ -26,10 +26,10 @@ if ( ! empty( $user_comments ) ) {
 			<div class="weal-top-comments">
 				<h4><?php esc_html_e( 'Top Comments', 'weal-profile' ); ?></h4>
 				<ul>
-					<?php foreach ( $user_comments as $user_comment ) : ?>
+					<?php foreach ( $weal_profile_user_comments as $weal_profile_user_comment ) : ?>
 						<li>
-							<a href="<?php echo esc_url( get_permalink( $user_comment->comment_post_ID ) ); ?>#comment-<?php echo esc_attr( $user_comment->comment_ID ); ?>">
-								<?php echo esc_html( wp_trim_words( $user_comment->comment_content, 10, '...' ) ); ?>
+							<a href="<?php echo esc_url( get_permalink( $weal_profile_user_comment->comment_post_ID ) ); ?>#comment-<?php echo esc_attr( $weal_profile_user_comment->comment_ID ); ?>">
+								<?php echo esc_html( wp_trim_words( $weal_profile_user_comment->comment_content, 10, '...' ) ); ?>
 							</a>
 
 						</li>
