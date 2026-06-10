d86975bdc436d8de97e3b4921db8ac7b6365c3bd
leouix <nsht22sola@gmail.com>
2026-06-11 02:30:43 +0700

    achievements: replace dynamic description with static label in badge icon rendering

--- includes/achievements/class-weal-profile-achievements.php
+++ includes/achievements/class-weal-profile-achievements.php
@@ -359,11 +359,10 @@ class Weal_Profile_Achievements implements Weal_Profile_Module_Singleton_Interfa
 				continue;
 			}
 
-			$description = self::get_achievement_description( $id, $settings['target'] );
 			$icon        = isset( $settings['icon'] ) ? $settings['icon'] : '';
 
 			$badge_class  = 'has-badge-' . $id;
-			$badges_html .= self::render_achievement_icon( $icon, $badge_class, $description );
+			$badges_html .= self::render_achievement_icon( $icon, $badge_class, $settings['label'] );
 		}
 
 		if ( '' === $badges_html ) {
